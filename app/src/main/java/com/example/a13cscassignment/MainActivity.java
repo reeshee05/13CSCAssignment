5f5160e65ab1f7ece6053e06013ed9872cdc6b25
Rishi <18134@students.mrgs.school.nz>
2022-07-19 17:37:42 +1200

    Register And Tweaks

--- app/src/main/java/com/example/a13cscassignment/MainActivity.java
+++ app/src/main/java/com/example/a13cscassignment/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         signup = (TextView) findViewById(R.id.signup);
         signup.setOnClickListener(this);
+
     }
 
     @Override
--- app/src/main/java/com/example/a13cscassignment/RegisterUser.java
+++ app/src/main/java/com/example/a13cscassignment/RegisterUser.java
@@ -10,5 +10,6 @@ public class RegisterUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
     }
 }
