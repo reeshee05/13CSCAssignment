6981f222db17349d0374a009fb7f03402bcbc83e
Rishi <18134@students.mrgs.school.nz>
2022-07-19 17:33:38 +1200

    Register and Tweaks

--- app/src/main/java/com/example/a13cscassignment/MainActivity.java
+++ app/src/main/java/com/example/a13cscassignment/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.a13cscassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = (TextView) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.signup:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
