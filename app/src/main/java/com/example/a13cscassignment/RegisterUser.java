2e4b1d70baf17e28422577012631540975639047
Rishi <18134@students.mrgs.school.nz>
2022-07-20 13:20:13 +1200

    Requirements for Signing Up and Data Collection

--- app/src/main/java/com/example/a13cscassignment/RegisterUser.java
+++ app/src/main/java/com/example/a13cscassignment/RegisterUser.java
@@ -1,20 +1,27 @@
 package com.example.a13cscassignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView signin, greeting, email_title, password_title, forgotpass, signup, name_title;
+    private TextView signin, continuebtn;
     private EditText editTextemail, editTextpassword, editTextuser_name;
     private ProgressBar progressBar;
 
@@ -30,8 +37,8 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
         signin = (TextView) findViewById(R.id.signin);
         signin.setOnClickListener(this);
 
-        signup = (TextView) findViewById(R.id.signup);
-        signup.setOnClickListener(this);
+        continuebtn = (Button) findViewById(R.id.signup);
+
 
         editTextuser_name = (EditText) findViewById(R.id.user_name);
         editTextemail = (EditText) findViewById(R.id.email);
@@ -41,18 +48,18 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()){
+    public void onClick(View v) {
+        switch (v.getId()){
             case R.id.signin:
                 startActivity(new Intent(this, MainActivity.class));
                 break;
-            case R.id.signup:
-                signup();
+            case R.id.continuebtn:
+                continuebtn();
                 break;
         }
     }
 
-    private void signup() {
+    private void continuebtn() {
         String email = editTextemail.getText().toString().trim();
         String password = editTextpassword.getText().toString().trim();
         String user_name = editTextuser_name.getText().toString().trim();
@@ -86,5 +93,39 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
             editTextpassword.requestFocus();
             return;
         }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password);
+                continuebtn.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            User user = new User(user_name, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if(taskisSuccessful()) {
+                                                Toast.makeText(RegisterUser.this, "User has been registered!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.VISIBLE);
+
+                                                // redirect to login layout
+                                            }else{
+                                                Toast.makeText(RegisterUser.this, "Failed to register", Toast.LENGTH_SHORT).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                                }else{
+                                    Toast.makeText(RegisterUser.this, "Failed to register", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.GONE);
+            }
+        }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/a13cscassignment/User.java
@@ -0,0 +1,16 @@
+package com.example.a13cscassignment;
+
+public class User {
+
+    public String user_name, email;
+
+    public User(){
+
+    }
+
+    public User(String user_name, String email){
+        this.user_name = user_name;
+        this.email = email;
+    }
+
+}
